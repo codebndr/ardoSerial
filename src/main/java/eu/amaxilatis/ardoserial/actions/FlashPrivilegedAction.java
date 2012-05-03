2cc9f9459e28a596740c8014a4df8512f15d662b
amaxilat <amaxilat@cti.gr>
2012-05-03 17:19:21 +0300

    blah

--- src/main/java/eu/amaxilatis/ardoserial/actions/FlashPrivilegedAction.java
+++ src/main/java/eu/amaxilatis/ardoserial/actions/FlashPrivilegedAction.java
@@ -37,6 +37,50 @@ public class FlashPrivilegedAction implements PrivilegedAction {
     }
 
     private Object flashWindows() {
+        int retval = checkAvrdudeWindows();
+        String avrdudePath;
+        if (retval > 0) {
+            avrdudePath = "C:\\Temp\\ ";
+        } else {
+            return null;
+        }
+        if (!checkAvrdudeConfWindows()) {
+            return null;
+        }
+        try {
+            FileWriter fileWriter = null;
+            fileWriter = new FileWriter("C:\\Temp\\file.hex");
+            fileWriter.write(file);
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        StringBuilder flashCommand = new StringBuilder();
+
+        flashCommand.append(avrdudePath)
+                .append(" -C C:\\Temp\\avrdude.conf ")
+                .append(" -P ").append(port)
+                .append(" -c stk500v1 ")
+                .append(" -p m328p ")
+                .append(" -u -U flash:w:").append("C:\\Temp\\file.hex")
+                .append(" -b ").append(baudRate)
+                .append(" -F");
+
+
+        try {
+            LOGGER.info("running : " + flashCommand.toString());
+            Process flashProc = Runtime.getRuntime().exec(flashCommand.toString());
+            try {
+                flashProc.waitFor();
+            } catch (InterruptedException e) {
+                LOGGER.error(e);
+            }
+            System.out.println("flashed");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -170,6 +214,36 @@ public class FlashPrivilegedAction implements PrivilegedAction {
         return true;
     }
 
+    public boolean checkAvrdudeConfWindows() {
+        File avrdudeConf = new File("C:\\Temp\\avrdude.conf");
+        if (!avrdudeConf.exists()) {
+            LOGGER.info("avrdude.conf does not exist");
+            InputStream input = this.getClass().getResourceAsStream("/avrdude.conf.linux");
+            InputStreamReader reader = new InputStreamReader(input);
+            BufferedWriter writer = null;
+
+            try {
+                writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(avrdudeConf)));
+            } catch (FileNotFoundException e) {
+                LOGGER.error(e);
+                return false;
+            }
+            final BufferedReader bufferedReader = new BufferedReader(reader);
+            try {
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    writer.write(line + "\n");
+                    line = bufferedReader.readLine();
+                }
+                writer.close();
+            } catch (IOException e) {
+                LOGGER.error(e);
+                return false;
+            }
+
+        }
+        return true;
+    }
 
     public boolean checkAvrdudeConfMac() {
         File avrdudeConf = new File("/tmp/avrdude.conf");
@@ -230,6 +304,46 @@ public class FlashPrivilegedAction implements PrivilegedAction {
                 input.close();
                 output.close();
                 Process chmodProcess = Runtime.getRuntime().exec("chmod u+x /tmp/avrdude");
+
+            } else {
+                return 2;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return 1;
+    }
+
+    public int checkAvrdudeWindows() {
+        try {
+            Process checkProcess = Runtime.getRuntime().exec("dir C:\\Temp\\avrdude.exe");
+            try {
+                checkProcess.waitFor();
+            } catch (InterruptedException e) {
+                LOGGER.error(e);
+            }
+            InputStreamReader stream = new InputStreamReader(checkProcess.getInputStream());
+            final BufferedReader reader = new BufferedReader(stream);
+            String line = reader.readLine();
+            if (line == null) {
+                InputStream input = this.getClass().getResourceAsStream("/bins/avrdude.exe");
+                FileOutputStream output;
+                try {
+                    output = new FileOutputStream(new File("C:\\Temp\\avrdude.exe"));
+                } catch (FileNotFoundException e) {
+                    LOGGER.error(e);
+                    return -1;
+                }
+                int c;
+
+                while ((c = input.read()) != -1) {
+                    output.write(c);
+                }
+                input.close();
+                output.close();
+                Process chmodProcess = Runtime.getRuntime().exec("CACLS /p /e %USERNAME%:f ");
 
             } else {
                 return 2;
